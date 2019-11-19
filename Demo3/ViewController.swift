6643c6436d9913611592c310d9c82045c75ed547
peterpan <apppeterpan@gmail.com>
2019-11-19 14:08:52 +0800

    update

--- Demo3/ViewController.swift
+++ Demo3/ViewController.swift
@@ -7,12 +7,33 @@
 //
 
 import UIKit
+import FacebookCore
+import Kingfisher
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+   
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        if let _ = AccessToken.current {
+            Profile.loadCurrentProfile { (profile, error) in
+                if let profile = profile {
+                    self.label.text = profile.name
+                    let url = profile.imageURL(forMode: .square, size: CGSize(width: 300, height: 300))
+                    self.imageView.kf.setImage(with: url)
+                  
+                }
+            }
+        }
     }
 
 
